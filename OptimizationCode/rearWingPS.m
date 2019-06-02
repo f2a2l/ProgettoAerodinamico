6611cb97b76f880da003ce278a82a0dd182c62cc
fabio <semerarofabio@gmail.com>
2019-06-03 01:44:42 +0200

    Added Possible Optimization Script

---
+++ OptimizationCode/rearWingPS.m
@@ -0,0 +1,12 @@
+%% Particle Swarm Optimization Procedure for F1 Rear Wing Design
+
+fun = @lapTime;
+nvars = 21;
+optiPAR; %as output: lb and ub. Here it is possibile to define boundaries for optimization
+
+
+options = optimoptions('particleswarm','SwarmSize',50,'Display','Iter','FunctionTolerance',1e-6,...
+                        'MaxIterations',200*nvars,'MaxTime',60*60*24,'PlotFcn','pswplotbestf',...
+                        'UseParallel','True')
+                    
+[optimalSolution] = particleswarm(fun,nvars,lb,ub);
