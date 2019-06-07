30e24c247cca922d04a35f92884127f5ea228f25
Andrea Andreolli <andreavalerio.andreolli@gmail.com>
2019-06-07 19:45:56 +0200

    Changed directory name.

--- addPaths.m
+++ addPaths.m
@@ -1,7 +1,7 @@
 % This scripts adds all the subfolders to MATLAB path, so that any function inside them can
 % can be accessed from parent directory. Please run this script at the beginning of any session.
 
-addpath('HessSmith')
+addpath('ViscousHessSmith')
 addpath('Lap_Performance')
 addpath('Geometry')
 addpath('2dto3dcorrection')
