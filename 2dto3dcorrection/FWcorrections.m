d8b85ad57e32b0aadeecc82a9e99020ec0ac590b
fabio <semerarofabio@gmail.com>
2019-06-07 23:01:22 +0200

    modified for DRS-ON & DRS-OFF conf.

--- 2dto3dcorrection/FWcorrections.m
+++ 2dto3dcorrection/FWcorrections.m
@@ -8,16 +8,16 @@ function[Cl_new,Cd_new] = FWcorrections(Cl,Cd_0,lambda,b,h)
 % s = correction factor
 % t = correction factor
 
-Cl = sum(Cl);
-Cd_0 = sum(Cd_0);
+Cl = sum(Cl');
+Cd_0 = sum(Cd_0');
 
 [t,s] = corr2dto3d(lambda);
 
 % endplates correction
 lambda_n = lambda * (1 + 1.9 * h / b);
 % 3D correction
-Cl_new = Cl / (1 + 2 / lambda_n);
-Cd_new = Cd_0 + (1 / (pi * lambda_n)) * Cl_new^2;
+Cl_new = Cl ./ (1 + 2 / lambda_n);
+Cd_new = Cd_0 + (1 / (pi * lambda_n)) * Cl_new.^2;
 % rectangular wing correction
-Cl_new = Cl_new * (1 + t) ;
-Cd_new = Cd_new * (1 + s) ;
+Cl_new = Cl_new .* (1 + t) ;
+Cd_new = Cd_new .* (1 + s) ;
