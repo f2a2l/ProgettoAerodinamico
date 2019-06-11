7da13b56d123ac0c686cd6bedddb0e98ca73360c
fabio <semerarofabio@gmail.com>
2019-06-11 23:53:36 +0200

    updated paths

--- addPaths.m
+++ addPaths.m
@@ -1,8 +1,13 @@
 % This scripts adds all the subfolders to MATLAB path, so that any function inside them can
 % can be accessed from parent directory. Please run this script at the beginning of any session.
 
-addpath('HessSmith')
-addpath('Lap_Performance')
-addpath('Geometry')
-addpath('2dto3dcorrection')
-addpath('OptimizationCode/')
+currDir = pwd;
+HSfolder = strcat(currDir,'/HessSmith');
+XFfolder = strcat(currDir,'/XFOIL');
+OPTIfolder = strcat(currDir,'/OptimizationCode');
+LAPfolder = strcat(currDir,'/Lap_Performance');
+GEOMfolder = strcat(currDir,'/Geometry');
+DYNfolder = strcat(currDir,'/DynMod');
+CORRfolder = strcat(currDir,'/2dto3dcorrection');
+CFDfolder = strcat(currDir,'/CFD');
+addpath(HSfolder,XFfolder,LAPfolder,GEOMfolder,DYNfolder,CORRfolder,OPTIfolder,CFDfolder);
